9c1ed0b6188fac6f1b45ef1dda0d3ad99ff9102d
Dmitry <dmitry.lyovochkin@gmail.com>
2021-10-09 18:29:48 +0300

    Добавление альбомной ориентации words_page

--- lib/widgets/main_screen/words_page.dart
+++ lib/widgets/main_screen/words_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_application/widgets/main_screen/main_screen_widget.dart';
+import 'package:flutter/services.dart';
 
 class WordsPageWidget extends StatefulWidget {
   const WordsPageWidget({Key? key}) : super(key: key);
@@ -7,6 +8,7 @@ class WordsPageWidget extends StatefulWidget {
   
   @override
   _WordsPageWidgetState createState() => _WordsPageWidgetState();
+  
 }
 
 class _WordsPageWidgetState extends State<WordsPageWidget> {
@@ -25,4 +27,21 @@ class _WordsPageWidgetState extends State<WordsPageWidget> {
        ),
     );
   }
+  @override
+    void initState(){
+      super.initState();
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
+  }
+  @override
+  dispose(){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 }
+
