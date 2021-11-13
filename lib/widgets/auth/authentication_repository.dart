c82d711f8d7e2d1f388848871e2689bd5bb35ead
Dmitry <dmitry.lyovochkin@gmail.com>
2021-11-13 21:42:24 +0300

    часть версии авторизации по туториалу доки от Felix

--- lib/widgets/auth/authentication_repository.dart
+++ lib/widgets/auth/authentication_repository.dart
@@ -1,4 +1,4 @@
 library authentication_repository;
 
 export 'package:english_application/widgets/auth/authentication_repository.dart';
-export 'src/models/models.dart';
+// export 'src/models/models.dart';
