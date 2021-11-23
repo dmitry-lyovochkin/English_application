1e0d04becbe5f36a86617e6f17610ef164536b64
Dmitry <dmitry.lyovochkin@gmail.com>
2021-11-23 19:43:59 +0300

    Полурабочая версия слайдера с показом картинок

--- lib/widgets/main_screen/suggestion_page/suggestions_page.dart
+++ lib/widgets/main_screen/suggestion_page/suggestions_page.dart
@@ -1,134 +1,177 @@
- import 'dart:io';
+//  import 'dart:io';
 
- import 'package:carousel_slider/carousel_slider.dart';
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
- import 'package:firebase_storage/firebase_storage.dart';
- import 'package:flutter/material.dart';
+//  import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+//  import 'package:firebase_storage/firebase_storage.dart';
+//  import 'package:flutter/material.dart';
 
- firebase_storage.FirebaseStorage storage =
-   firebase_storage.FirebaseStorage.instance;
+//  firebase_storage.FirebaseStorage storage =
+//    firebase_storage.FirebaseStorage.instance;
 
- 
+// final List<String> imagesList = [
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
+// ];
 
-final List<String> imagesList = [
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/dread.jpg?alt=media&token=9e75ec06-559d-4f5c-af9c-eaed463591c6',
-];
+// final List<String> titles = [
+//   ' Coffee ',
+//   ' Bread ',
+//   ' Gelato ',
+//   ' Ice Cream ',
+// ];
 
-final List<String> titles = [
-  ' Coffee ',
-  ' Bread ',
-  ' Gelato ',
-  ' Ice Cream ',
-];
+// void main() {
+//   runApp(const MyApp1());
+// }
 
-void main() {
-  runApp(const MyApp1());
-}
+// class MyApp1 extends StatefulWidget {
+//   const MyApp1({Key? key}) : super(key: key);
 
-class MyApp1 extends StatefulWidget {
-  const MyApp1({Key? key}) : super(key: key);
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+// class _MyAppState extends State<MyApp1> {
+//   int _currentIndex = 0;
 
-class _MyAppState extends State<MyApp1> {
-  int _currentIndex = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Carousel with indicator demo'),
+//         ),
+//         body: Column(
+//           children: [
+//             CarouselSlider(
+//               options: CarouselOptions(
+//                 autoPlay: true,
+//                 // enlargeCenterPage: true,
+//                 //scrollDirection: Axis.vertical,
+//                 onPageChanged: (index, reason) {
+//                   setState(
+//                     () {
+//                       _currentIndex = index;
+//                     },
+//                   );
+//                 },
+//               ),
+//               items: imagesList
+//                   .map(
+//                     (item) => Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: Card(
+//                         margin: EdgeInsets.only(
+//                           top: 10.0,
+//                           bottom: 10.0,
+//                         ),
+//                         elevation: 6.0,
+//                         shadowColor: Colors.redAccent,
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(30.0),
+//                         ),
+//                         child: ClipRRect(
+//                           borderRadius: BorderRadius.all(
+//                             Radius.circular(30.0),
+//                           ),
+//                           child: Stack(
+//                             children: <Widget>[
+//                               Image.network(
+//                                 item,
+//                                 fit: BoxFit.cover,
+//                                 width: double.infinity,
+//                               ),
+//                               Center(
+//                                 child: Text(
+//                                   '${titles[_currentIndex]}',
+//                                   style: TextStyle(
+//                                     fontSize: 24.0,
+//                                     fontWeight: FontWeight.bold,
+//                                     backgroundColor: Colors.black45,
+//                                     color: Colors.white,
+//                                   ),
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   )
+//                   .toList(),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: imagesList.map((urlOfItem) {
+//                 int index = imagesList.indexOf(urlOfItem);
+//                 return Container(
+//                   width: 10.0,
+//                   height: 10.0,
+//                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+//                   decoration: BoxDecoration(
+//                     shape: BoxShape.circle,
+//                     color: _currentIndex == index
+//                         ? Color.fromRGBO(0, 0, 0, 0.8)
+//                         : Color.fromRGBO(0, 0, 0, 0.3),
+//                   ),
+//                 );
+//               }).toList(),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Carousel with indicator demo'),
-        ),
-        body: Column(
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                autoPlay: true,
-                // enlargeCenterPage: true,
-                //scrollDirection: Axis.vertical,
-                onPageChanged: (index, reason) {
-                  setState(
-                    () {
-                      _currentIndex = index;
-                    },
-                  );
-                },
-              ),
-              items: imagesList
-                  .map(
-                    (item) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        margin: EdgeInsets.only(
-                          top: 10.0,
-                          bottom: 10.0,
-                        ),
-                        elevation: 6.0,
-                        shadowColor: Colors.redAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(30.0),
-                          ),
-                          child: Stack(
-                            children: <Widget>[
-                              Image.network(
-                                item,
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                              ),
-                              Center(
-                                child: Text(
-                                  '${titles[_currentIndex]}',
-                                  style: TextStyle(
-                                    fontSize: 24.0,
-                                    fontWeight: FontWeight.bold,
-                                    backgroundColor: Colors.black45,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: imagesList.map((urlOfItem) {
-                int index = imagesList.indexOf(urlOfItem);
-                return Container(
-                  width: 10.0,
-                  height: 10.0,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentIndex == index
-                        ? Color.fromRGBO(0, 0, 0, 0.8)
-                        : Color.fromRGBO(0, 0, 0, 0.3),
-                  ),
-                );
-              }).toList(),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+// // import 'package:cloud_firestore/cloud_firestore.dart';
+// // import 'package:flutter/material.dart';
 
+// // class MyApp1 extends StatelessWidget {
+// //   const MyApp1({Key? key}) : super(key: key);
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return const MaterialApp(
+// //       home: FirestoreSlideshow(),
+// //     );
+// //   }
+// // }
+
+// // class FirestoreSlideshow extends StatefulWidget {
+// //   const FirestoreSlideshow({Key? key}) : super(key: key);
+
+// //   @override
+// //   _FirestoreSlideshowState createState() => _FirestoreSlideshowState();
+// // }
+
+// // class _FirestoreSlideshowState extends State<FirestoreSlideshow> {
+// //   Stream slides;
+// //   Stream _queryDb() {
+// //     slides = FirebaseFirestore.instance
+// //         .collection('stores')
+// //         .snapshots()
+// //         .map((list) => list.docs.map((doc) => doc.data()));
+// //   }
+
+// //   @override
+// //     void initState() {
+// //       _queryDb();
+// //       super.initState();
+// //     }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       body: StreamBuilder(
+// //         stream: slides,
+// //         builder: (context, AsyncSnapshot snap),),
+// //     );
+// //   }
+// // }
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:flutter/material.dart';
 
@@ -138,7 +181,7 @@ class _MyAppState extends State<MyApp1> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return const MaterialApp(
-//       home: FirestoreSlideshow(),
+//       home: Scaffold(body: FirestoreSlideshow()),
 //     );
 //   }
 // }
@@ -147,30 +190,137 @@ class _MyAppState extends State<MyApp1> {
 //   const FirestoreSlideshow({Key? key}) : super(key: key);
 
 //   @override
-//   _FirestoreSlideshowState createState() => _FirestoreSlideshowState();
+//   FirestoreSlideshowState createState() => FirestoreSlideshowState();
 // }
 
-// class _FirestoreSlideshowState extends State<FirestoreSlideshow> {
+// class FirestoreSlideshowState extends State<FirestoreSlideshow> {
+//   final PageController ctrl = PageController(viewportFraction: 0.8);
+
+//   final Firestore db = Firestore.instance;
 //   Stream slides;
-//   Stream _queryDb() {
-//     slides = FirebaseFirestore.instance
-//         .collection('stores')
-//         .snapshots()
-//         .map((list) => list.docs.map((doc) => doc.data()));
+
+//   String activateTAg = 'davourites';
+
+//   int currentPage = 0;
+
+//   @override
+//   void initstate() {
+//     _queryDb();
+
+//     ctrl.addListener(() {
+//       final next = ctrl.page!.round();
+
+//       if (currentPage != next) {
+//         setState(() {
+//           currentPage = next;
+//         });
+//       }
+//     });
 //   }
 
 //   @override
-//     void initState() {
-//       _queryDb();
-//       super.initState();
-//     }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: StreamBuilder(
-//         stream: slides,
-//         builder: (context, AsyncSnapshot snap),),
-//     );
-//   }
+//   Widget build(BuildContext context) {}
 // }
+
+// Stream<dynamic> _queryDb({String tag = 'favourites'}) {
+//   Query query = db.collection('stories').where('tags', arrayComtains: tag);
+
+//   slides = query.snapshots().map((list) => list.documents.map((doc) => doc.data));
+
+//   setState(() {
+//     activeTag = tag;
+//   });
+// }
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirestoreSlideshow(),
+    );
+  }
+}
+
+class FirestoreSlideshow extends StatefulWidget {
+  const FirestoreSlideshow({Key? key}) : super(key: key);
+
+  @override
+  _FirestoreSlideshowState createState() => _FirestoreSlideshowState();
+}
+
+class _FirestoreSlideshowState extends State<FirestoreSlideshow> {
+  Stream? slides;
+
+  Stream _queryDb() {
+    slides = FirebaseFirestore.instance
+        .collection('stories')
+        .snapshots()
+        .map((list) => list.docs.map((doc) => doc.data()));
+  }
+
+  @override
+  void initState() {
+    _queryDb();
+    super.initState();
+  }
+
+  final PageController controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: slides,
+          builder: (context, AsyncSnapshot snap) {
+            final List slideList = snap.data.toList();
+
+            if (snap.hasError) {
+              return Text(snap.error);
+            }
+            if (snap.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return PageView.builder(
+              controller: controller,
+                itemCount: slideList.length,
+                itemBuilder: (context, int index) {
+                  return _BuildStorePage(slideList[index]);
+                });
+          }),
+    );
+  }
+
+    Widget _BuildStorePage(Map<dynamic, dynamic> data) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(data['img']),
+        ),
+      ),
+      child: Center(
+        child: Text(
+          data['title'],
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+          ),
+        ),
+      ),
+
+    ),
+  }
+}
