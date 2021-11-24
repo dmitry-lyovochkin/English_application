a12804bb523b72779b2c7ba348660e4b27581cea
Dmitry <dmitry.lyovochkin@gmail.com>
2021-11-24 21:10:37 +0300

    Работа над слайдом. Добавил копки переключения и номера слайдов

--- lib/widgets/main_screen/slider.dart
+++ lib/widgets/main_screen/slider.dart
@@ -1,8 +1,83 @@
+// import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:flutter/material.dart';
+
+// final List<String> imgList = [
+//   // 'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdread.jpg?alt=media&token=449e01fd-c51a-47e9-b45e-878ff4b08588',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdrought.jpg?alt=media&token=5da7e5cc-ec0a-4eb0-9386-caaa0ffdeada',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Ffine.jpg?alt=media&token=280debb3-f6c1-4c9b-aaa3-1bb4eb0d6a4e',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fgoal.jpg?alt=media&token=2677924e-0296-4cb2-8938-ef6f16bacafd',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fgrab.jpg?alt=media&token=f5ed0e6a-ea8f-4699-8c2e-502d804526b5',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Flook%20after.jpg?alt=media&token=1e57f321-1837-4a8e-9d99-936009365781',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fmug.jpg?alt=media&token=4a265f7c-1b31-40f3-a729-977b1737ce6b',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Frule.jpg?alt=media&token=425e94f4-912b-409a-9e98-965c760c0656',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fsloppy.jpg?alt=media&token=a6151045-1655-4a89-8e7c-20a608cfc886',
+//   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fwall.jpg?alt=media&token=fa41d6f8-3eea-4137-9aae-da9646a79656',
+// ];
+
+// void main() => runApp(const CarouselDemo1());
+
+// final themeMode = ValueNotifier(2);
+
+// class CarouselDemo1 extends StatelessWidget {
+//   const CarouselDemo1({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ValueListenableBuilder(
+//       builder: (context, value, g) {
+//         return MaterialApp(
+//           // initialRoute: '/fullscreen',
+//           // darkTheme: ThemeData.dark(),
+//           themeMode: ThemeMode.values.toList()[value as int],
+//           debugShowCheckedModeBanner: false,
+//           home: const FullscreenSliderDemo(),
+//           // routes: {
+//           //   '/fullscreen': (ctx) => const FullscreenSliderDemo(),
+//           // },
+//         );
+//       },
+//       valueListenable: themeMode,
+//     );
+//   }
+// }
+
+// class FullscreenSliderDemo extends StatelessWidget {
+//   const FullscreenSliderDemo({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       // appBar: AppBar(title: Text('Fullscreen sliding carousel demo')),
+//       body: Builder(
+//         builder: (context) {
+//           // final height = MediaQuery.of(context).size.height;
+//           return CarouselSlider(
+//             options: CarouselOptions(
+//               height: 600,
+//               viewportFraction: 1,
+//               // autoPlay: false,
+//             ),
+//             items: imgList
+//                 .map((item) => Center(
+//                         child: Image.network(
+//                       item,
+//                       fit: BoxFit.cover,
+//                       // height: 500,
+//                       // width: 600,
+//                     )))
+//                 .toList(),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:english_application/theme.dart';
 import 'package:flutter/material.dart';
 
 final List<String> imgList = [
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdread.jpg?alt=media&token=449e01fd-c51a-47e9-b45e-878ff4b08588',
   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdrought.jpg?alt=media&token=5da7e5cc-ec0a-4eb0-9386-caaa0ffdeada',
   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Ffine.jpg?alt=media&token=280debb3-f6c1-4c9b-aaa3-1bb4eb0d6a4e',
   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fgoal.jpg?alt=media&token=2677924e-0296-4cb2-8938-ef6f16bacafd',
@@ -14,7 +89,7 @@ final List<String> imgList = [
   'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fwall.jpg?alt=media&token=fa41d6f8-3eea-4137-9aae-da9646a79656',
 ];
 
-void main() => runApp(const CarouselDemo1());
+// void main() => runApp(CarouselDemo());
 
 final themeMode = ValueNotifier(2);
 
@@ -26,14 +101,13 @@ class CarouselDemo1 extends StatelessWidget {
     return ValueListenableBuilder(
       builder: (context, value, g) {
         return MaterialApp(
-          // initialRoute: '/fullscreen',
+          initialRoute: '/',
           // darkTheme: ThemeData.dark(),
-          themeMode: ThemeMode.values.toList()[value as int],
+          // themeMode: ThemeMode.values.toList()[value as int],
           debugShowCheckedModeBanner: false,
-          home: const FullscreenSliderDemo(),
-          // routes: {
-          //   '/fullscreen': (ctx) => const FullscreenSliderDemo(),
-          // },
+          routes: {
+            '/': (ctx) =>  const ManuallyControlledSlider(),
+          },
         );
       },
       valueListenable: themeMode,
@@ -41,50 +115,126 @@ class CarouselDemo1 extends StatelessWidget {
   }
 }
 
-class DemoItem extends StatelessWidget {
-  const DemoItem(this.title, this.route, {Key? key}) : super(key: key);
-  final String title;
-  final String route;
+final List<Widget> imageSliders = imgList
+    .map((item) => Center(
+        child: ClipRRect(
+            child: Center(
+              child: Stack(
+                children: <Widget>[
+                  Image.network(
+                    item, 
+                    fit: BoxFit.cover, 
+                    height: 340, 
+                    width: 650
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    // child: Container(
+                    //   decoration: const BoxDecoration(
+                    //     gradient: LinearGradient(
+                    //       colors: [
+                    //         Color.fromARGB(200, 0, 0, 0),
+                    //         Color.fromARGB(0, 0, 0, 0)
+                    //       ],
+                    //       begin: Alignment.bottomCenter,
+                    //       end: Alignment.topCenter,
+                    //     ),
+                    //   ),
+                      // padding: const EdgeInsets.symmetric(
+                      //     vertical: 10, horizontal: 20),
+                      child: Text(
+                        '"Слово". ${imgList.indexOf(item)}',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 10,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                
+                ],
+              ),
+            )),
+      ),
+    )
+    .toList();
+
+
+
+class ManuallyControlledSlider extends StatefulWidget {
+  const ManuallyControlledSlider({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      // title: Text(title),
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
-    );
+  State<StatefulWidget> createState() {
+    return _ManuallyControlledSliderState();
   }
 }
 
-class FullscreenSliderDemo extends StatelessWidget {
-  const FullscreenSliderDemo({Key? key}) : super(key: key);
+class _ManuallyControlledSliderState extends State<ManuallyControlledSlider> {
+  final CarouselController _controller = CarouselController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text('Fullscreen sliding carousel demo')),
-      body: Builder(
-        builder: (context) {
-          // final height = MediaQuery.of(context).size.height;
-          return CarouselSlider(
-            options: CarouselOptions(
-              height: 600,
-              viewportFraction: 1,
-              // autoPlay: false,
-            ),
-            items: imgList
-                .map((item) => Center(
-                        child: Image.network(
-                      item,
-                      fit: BoxFit.cover,
-                      // height: 500,
-                      // width: 600,
-                    )))
-                .toList(),
-          );
-        },
-      ),
-    );
+        // appBar: AppBar(title: Text('Manually controlled slider')),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              CarouselSlider(
+                items: imageSliders,
+                options: CarouselOptions(
+                  enlargeCenterPage: true, 
+                  viewportFraction: 1,
+                  height: 400
+                  
+                ),
+                carouselController: _controller,
+              ),
+              Padding(
+                padding:  const EdgeInsets.symmetric(
+                  vertical: 5, 
+                  horizontal: 5,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: IconButton(
+                        onPressed: _controller.previousPage,
+                        icon: const Icon(Icons.arrow_back),
+                        color: AppColors.mainColorApp,
+                      ),
+                    ),
+                    Flexible(
+                      child: IconButton(
+                        onPressed: _controller.nextPage,
+                        icon: const Icon(Icons.arrow_forward),
+                        color: AppColors.mainColorApp,
+                      ),
+                    ),
+                    ...Iterable<int>.generate(imgList.length).map(
+                      (int pageIndex) => Flexible(
+                        child: ElevatedButton(
+                          
+                          onPressed: () => _controller.animateToPage(pageIndex),
+                          child: Text('$pageIndex'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
+
+
--- lib/widgets/main_screen/words_page.dart
+++ lib/widgets/main_screen/words_page.dart
@@ -20,9 +20,10 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class PaintBoard extends StatefulWidget {
   const PaintBoard({Key? key}) : super(key: key);
- 
+
   @override
   _PaintBoardState createState() => _PaintBoardState();
 }
@@ -46,8 +47,9 @@ class _PaintBoardState extends State<PaintBoard> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
+
           children: [
-            const CarouselDemo1(),
+          const CarouselDemo1(),
             GestureDetector(
               onPanStart: (details) {
                 setState(() {
@@ -89,7 +91,6 @@ class _PaintBoardState extends State<PaintBoard> {
                   width: MediaQuery.of(context).size.width,
                 ),
               ),
-              
             ),
             // Нужно будет разделить на логику и внешку
             Positioned(
@@ -98,7 +99,7 @@ class _PaintBoardState extends State<PaintBoard> {
               child: IconButton(
                 color: AppColors.mainColorApp,
                 key: const Key('homePage_logout_iconButton'),
-                icon: const Icon(Icons.arrow_back),
+                icon: const Icon(Icons.reply),
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -116,22 +117,22 @@ class _PaintBoardState extends State<PaintBoard> {
                       value: strokeWidth,
                       onChanged: (val) => setState(() => strokeWidth = val),
                     ),
-                    ElevatedButton.icon(
+                    TextButton.icon(
                       onPressed: () => setState(() => drawingPoints = []),
                       icon: const Icon(Icons.cancel),
                       label: const Text('Clear Board'),
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(
-                            const Color.fromRGBO(121, 104, 216, 100)),
+                            const Color.fromRGBO(255, 255, 255, 255)),
                         foregroundColor:
-                            MaterialStateProperty.all(Colors.white),
+                            MaterialStateProperty.all(const Color.fromRGBO(121, 104, 216, 100)),
                         textStyle: MaterialStateProperty.all(
                           const TextStyle(
                               fontSize: 16, fontWeight: FontWeight.w700),
                         ),
                         padding: MaterialStateProperty.all(
                             const EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 8)),
+                                horizontal: 10, vertical: 5)),
                       ),
                     ),
                   ],
@@ -147,7 +148,6 @@ class _PaintBoardState extends State<PaintBoard> {
                   children: List.generate(
                       colors.length, (index) => _useColor(colors[index])),
                 ))));
-                
   }
 
   Widget _useColor(Color color) {
@@ -197,9 +197,8 @@ class _PaintBoardState extends State<PaintBoard> {
 
 class _DrawingPainter extends CustomPainter {
   _DrawingPainter(this.drawingPoints);
-  
-  final List<DrawingPoint?> drawingPoints;
 
+  final List<DrawingPoint?> drawingPoints;
 
   List<Offset> offsetsList = [];
 
@@ -224,23 +223,12 @@ class _DrawingPainter extends CustomPainter {
 }
 
 class DrawingPoint {
-  DrawingPoint(
-    this.offset, 
-    this.paint
-  );
+  DrawingPoint(this.offset, this.paint);
   Offset offset;
   Paint paint;
-
 }
 
-
-
-
-
-
-
-
-// firebase. Потом разделить на логику 
+// firebase. Потом разделить на логику
 
 // firebase_storage.FirebaseStorage storage =
 //   firebase_storage.FirebaseStorage.instance;
@@ -252,98 +240,4 @@ class DrawingPoint {
 //       .list(const firebase_storage.ListOptions(maxResults: 10));
 //   // ...
 // }
-
-
-final List<String> imgList = [
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdread.jpg?alt=media&token=449e01fd-c51a-47e9-b45e-878ff4b08588',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fdrought.jpg?alt=media&token=5da7e5cc-ec0a-4eb0-9386-caaa0ffdeada',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Ffine.jpg?alt=media&token=280debb3-f6c1-4c9b-aaa3-1bb4eb0d6a4e',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fgoal.jpg?alt=media&token=2677924e-0296-4cb2-8938-ef6f16bacafd',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fgrab.jpg?alt=media&token=f5ed0e6a-ea8f-4699-8c2e-502d804526b5',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Flook%20after.jpg?alt=media&token=1e57f321-1837-4a8e-9d99-936009365781',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fmug.jpg?alt=media&token=4a265f7c-1b31-40f3-a729-977b1737ce6b',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Frule.jpg?alt=media&token=425e94f4-912b-409a-9e98-965c760c0656',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fsloppy.jpg?alt=media&token=a6151045-1655-4a89-8e7c-20a608cfc886',
-  'https://firebasestorage.googleapis.com/v0/b/english-app-9b2d0.appspot.com/o/ExamplePac%2Fwall.jpg?alt=media&token=fa41d6f8-3eea-4137-9aae-da9646a79656',
-];
-
-void main() => runApp(const CarouselDemo());
-
-final themeMode = ValueNotifier(2);
-
-class CarouselDemo extends StatelessWidget {
-  const CarouselDemo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      builder: (context, value, g) {
-        return MaterialApp(
-          // initialRoute: '/fullscreen',
-          // darkTheme: ThemeData.dark(),
-          themeMode: ThemeMode.values.toList()[value as int],
-          debugShowCheckedModeBanner: false,
-          home: const FullscreenSliderDemo(),
-          // routes: {
-          //   '/fullscreen': (ctx) => const FullscreenSliderDemo(),
-          // },
-        );
-      },
-      valueListenable: themeMode,
-    );
-  }
-}
-
-class DemoItem extends StatelessWidget {
-  const DemoItem(
-    this.title, this.route, 
-    {Key? key}) : super(key: key);
-  final String title;
-  final String route;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      // title: Text(title),
-      onTap: () {
-        Navigator.pushNamed(context, route);
-      },
-    );
-  }
-}
-
-
-
-class FullscreenSliderDemo extends StatelessWidget {
-  const FullscreenSliderDemo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(title: Text('Fullscreen sliding carousel demo')),
-      body: Builder(
-        builder: (context) {
-          // final height = MediaQuery.of(context).size.height;
-          return CarouselSlider(
-            options: CarouselOptions(
-              height: 200,
-              viewportFraction: 1,
-              // autoPlay: false,
-            ),
-            items: imgList
-                .map((item) => Center(
-                    child: Image.network(
-                  item,
-                  fit: BoxFit.cover,
-                  height: 500,
-                  // width: 600,
-                )))
-                .toList(),
-          );
-        },
-      ),
-    );
-  }
-}
-
 
