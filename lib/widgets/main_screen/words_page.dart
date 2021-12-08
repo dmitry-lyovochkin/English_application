2e9f4f52046d50a6281bb0241456fdd6e54e0aa5
Dmitry <dmitry.lyovochkin@gmail.com>
2021-12-08 22:09:30 +0300

    small fix

--- lib/widgets/main_screen/words_page.dart
+++ lib/widgets/main_screen/words_page.dart
@@ -102,81 +102,45 @@ class _PaintBoardState extends State<PaintBoard> {
           // const ButtonsExample(),
 
           // Нужно будет разделить на логику и внешку
-          // Positioned(
-          //     top: 5,
-          //     left: 5,
-          //     child: ElevatedButton(
-          //         onPressed: () {},
-          //         style: ElevatedButton.styleFrom(
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(16),
-          //           ),
-          //           // minimumSize: const Size(55, 45),
-          //           primary: ButtonColor.buttonColor,
-          //           padding: const EdgeInsets.symmetric(
-          //             vertical: 14,
-          //             horizontal: 14,
-          //           ),
-          //         ),
-          //         child: Row(
-          //           children: const [
-          //             Icon(MyIcons.home),
-          //           ],
-          //         ))),
-          // Positioned(
-          //     top: 5,
-          //     right: 5,
-          //     child: ElevatedButton.icon(
-          //       icon: const Icon(MyIcons.tick_circle),
-          //       label: const Text('Сохранить'),
-          //       onPressed: () {},
-          //       style: ElevatedButton.styleFrom(
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(16),
-          //           ),
-          //           padding: const EdgeInsets.all(13),
-          //           primary: ButtonColor.buttonColor,
-          //           onPrimary: Colors.white),
-          //     )),
-          // // Positioned(
-          // //   bottom: 20,
-          // //   right: 70,
-          // //   child: ElevatedButton(
-          // //     onPressed: () {},
-          // //     style: ElevatedButton.styleFrom(
-          // //         shape: RoundedRectangleBorder(
-          // //           borderRadius: BorderRadius.circular(16),
-          // //         ),
-          // //         padding: const EdgeInsets.symmetric(
-          // //           vertical: 13,
-          // //           horizontal: 13,
-          // //         ),
-          // //         primary: Colors.white,
-          // //         onPrimary: ButtonColor.buttonColor),
-          // //     child: const Icon(MyIcons.vector),
-          // //   ),
-          // // ),
-          // Positioned(
-          //     bottom: 20,
-          //     left: 20,
-          //     child: ElevatedButton(
-          //       onPressed: () {},
-          //       style: ElevatedButton.styleFrom(
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(16),
-          //           ),
-          //           padding: const EdgeInsets.symmetric(
-          //             vertical: 14,
-          //             horizontal: 14,
-          //           ),
-          //           primary: Colors.white,
-          //           onPrimary: ButtonColor.buttonColor),
-          //       child: const Icon(MyIcons.voice),
-          //     )
-          // ),
+          Positioned(
+              top: 5,
+              left: 5,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    // minimumSize: const Size(55, 45),
+                    primary: ButtonColor.buttonColor,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 14,
+                      horizontal: 14,
+                    ),
+                  ),
+                  child: Row(
+                    children: const [
+                      Icon(MyIcons.home),
+                    ],
+                  ))),
+          Positioned(
+              top: 5,
+              right: 5,
+              child: ElevatedButton.icon(
+                icon: const Icon(MyIcons.tick_circle),
+                label: const Text('Сохранить'),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    padding: const EdgeInsets.all(13),
+                    primary: ButtonColor.buttonColor,
+                    onPrimary: Colors.white),
+              )),
           // Positioned(
           //   bottom: 20,
-          //   right: 20,
+          //   right: 70,
           //   child: ElevatedButton(
           //     onPressed: () {},
           //     style: ElevatedButton.styleFrom(
@@ -184,14 +148,50 @@ class _PaintBoardState extends State<PaintBoard> {
           //           borderRadius: BorderRadius.circular(16),
           //         ),
           //         padding: const EdgeInsets.symmetric(
-          //           vertical: 14,
-          //           horizontal: 14,
+          //           vertical: 13,
+          //           horizontal: 13,
           //         ),
           //         primary: Colors.white,
           //         onPrimary: ButtonColor.buttonColor),
-          //     child: const Icon(MyIcons.vector__1_),
+          //     child: const Icon(MyIcons.vector),
           //   ),
           // ),
+          Positioned(
+              bottom: 20,
+              left: 20,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 14,
+                      horizontal: 14,
+                    ),
+                    primary: Colors.white,
+                    onPrimary: ButtonColor.buttonColor),
+                child: const Icon(MyIcons.voice),
+              )
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 14,
+                    horizontal: 14,
+                  ),
+                  primary: Colors.white,
+                  onPrimary: ButtonColor.buttonColor),
+              child: const Icon(MyIcons.vector__1_),
+            ),
+          ),
         ]));
   }
 
